c81e5fb5503fd7d4e7ef44dc7c7468ddf87a290b
Pethanan <peth.engr@gmail.com>
2023-07-29 23:41:36 +0530

    Login code added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const Expense = require("../models/expense");
 const User = require("../models/user");
+
 exports.getSignup = (req, res, next) => {
   res.render("auth/signup", { pageTitle: "Signup" });
 };
@@ -13,13 +14,17 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ where: { mail: mail } })
     .then((user) => {
       if (!user) {
-        return User.create({ name, mail, password });
+        User.create({ name, mail, password })
+          .then((user) => {
+            res.redirect("/auth/signup");
+          })
+          .catch((err) => console.log(err));
+        return;
       }
-      console.log("Usermail id aleady exists");
-      return user;
-    })
-    .then((user) => {
-      res.redirect("/auth/error");
+      res.render("auth/error", {
+        pageTitle: "Error",
+        error: "Mail Id already exists",
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -27,9 +32,32 @@ exports.postSignup = (req, res, next) => {
     });
 };
 
-exports.getError = (req, res, next) => {
-  res.render("auth/error", {
-    pageTitle: "Error",
-    error: "Duplicate Mail Id",
-  });
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", { pageTitle: "Login" });
+};
+
+exports.postLogin = (req, res, next) => {
+  const mail = req.body.mail;
+  const password = req.body.password;
+  const userLoginCredentials = { mail, password };
+  User.findOne({ where: { mail: mail } })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/error", {
+          pageTitle: "Authentication error",
+          error: "Mail Id does not exist, please signup",
+        });
+        return;
+      } else if (user.password !== password) {
+        res.render("auth/error", {
+          pageTitle: "Authentication error",
+          error: "InCorrect Password",
+        });
+        return;
+      }
+      res.render("auth/login", { pageTitle: "Login" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
-router.get("/error", authController.getError);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
 
 module.exports = router;
