0b82b28182b6c5231e15cb0931e07d8d88004e8c
Pethanan <peth.engr@gmail.com>
2023-08-08 23:51:52 +0530

    User expense relationship

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const User = require("./models/user");
+const Expense = require("./models/expense");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,8 +16,20 @@ const sequelize = require("./util/database");
 const expensesRoute = require("./routes/expenses");
 const authRoute = require("./routes/auth");
 
+app.use((req, res, next) => {
+  User.findByPk(2)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/auth", authRoute);
 app.use(expensesRoute);
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
 sequelize
   .sync()
