3904e7fb79f780c90584c93cdeb2bad630972ca7
Pethanan <peth.engr@gmail.com>
2023-09-18 16:36:16 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+const sequelize = require("./util/database");
 const User = require("./models/user");
 const Expense = require("./models/expense");
 const ForgotPasswordRequest = require("./models/forgotPasswordRequests");
 const Order = require("./models/order");
-const dotenv = require("dotenv");
-
-const app = express();
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-dotenv.config();
-app.use(express.static(path.join(__dirname, "public")));
-
-const sequelize = require("./util/database");
-
 const expenseRoute = require("./routes/expenses");
 const userRoute = require("./routes/user");
 const purchaseRoute = require("./routes/purchase");
 const premiumRoute = require("./routes/premium");
 const passwordRoute = require("./routes/password");
 
+const app = express();
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+// Middleware setup
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+dotenv.config();
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes setup
 app.use(userRoute);
 app.use(expenseRoute);
 app.use(purchaseRoute);
 app.use(premiumRoute);
 app.use(passwordRoute);
 
+// Associations
 User.hasMany(Expense);
 Expense.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 Order.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
@@ -39,11 +52,11 @@ ForgotPasswordRequest.belongsTo(User, {
 });
 User.hasMany(ForgotPasswordRequest);
 
+// Server initialization
 sequelize
   .sync()
   .then((result) => {
-    console.log(result);
-    app.listen(4000);
+    app.listen(process.env.PORT || 4000);
   })
   .catch((err) => {
     console.log(err);
--- util/database.js
+++ util/database.js
@@ -1,13 +1,15 @@
 const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const sequelize = new Sequelize(
-  "peth-expense-tracker-schema",
-  "root",
-  "PETHmysql1@",
-  {
-    dialect: "mysql",
-    host: "localhost",
-  }
-);
+const db_schema = process.env.DATABASE_SCHEMA;
+const user = process.env.DATABASE_USER;
+const password = process.env.DATABASE_PASSWORD;
+console.log(password);
+console.log(`${process.env.DATABASE_PASSWORD}`);
+const sequelize = new Sequelize(db_schema, user, password, {
+  dialect: "mysql",
+  host: "localhost",
+});
 
 module.exports = sequelize;
